154cc49e0cc5b996f40a691bf5401db173acf157
mp-apic <pichlermathias2003@gmail.com>
2026-04-09 20:55:51 +0200

    feat(dimensions): implement immutable DimensionRegistry and custom exceptions

---
+++ march-core/src/main/java/io/github/march_plugin/core/dimensions/exceptions/DimensionNotFoundException.java
@@ -0,0 +1,16 @@
+package io.github.march_plugin.core.dimensions.exceptions;
+
+/**
+ * Thrown when a rule is not specified in configuration.
+ */
+public class DimensionNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs the exception.
+     *
+     * @param dimensionName the name of the dimension that is not defined in the configuration.
+     */
+    public DimensionNotFoundException(final String dimensionName) {
+        super("Dimension '%s' is not defined in March Configuration.".formatted(dimensionName));
+    }
+}
