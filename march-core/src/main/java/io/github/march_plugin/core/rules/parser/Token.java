60f84740ea48dd3fc1e437f37fcf6892de7f60ae
mp-apic <pichlermathias2003@gmail.com>
2026-04-09 22:25:40 +0200

    feat(rules): define token types and rule parsing exceptions

---
+++ march-core/src/main/java/io/github/march_plugin/core/rules/parser/Token.java
@@ -0,0 +1,5 @@
+package io.github.march_plugin.core.rules.parser;
+
+record Token(TokenType type, String value) {
+
+}
---
+++ march-core/src/main/java/io/github/march_plugin/core/rules/parser/TokenType.java
@@ -0,0 +1,17 @@
+package io.github.march_plugin.core.rules.parser;
+
+enum TokenType {
+    IDENTIFIER,   // source.domain, target.layer
+    LITERAL,      // presentation, user, dto
+    EQUALS,       // ==
+    NOT_EQUALS,   // !=
+    AND,          // &&
+    OR,           // ||
+    OPEN_PAREN,   // (
+    CLOSE_PAREN,  // )
+    NULL,         // NULL
+    NOT,          // !
+    IN,           // IN
+    PIPE,         // |
+    EOF
+}
