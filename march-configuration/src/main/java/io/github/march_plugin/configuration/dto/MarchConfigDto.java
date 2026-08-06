1b6ab1c494312edf429a45d4ce58b77c371e5826
mp-apic <pichlermathias2003@gmail.com>
2026-08-06 23:31:04 +0200

    feat(march config dto): Add march config dto

--- march-configuration/src/main/java/io/github/march_plugin/configuration/dto/MarchConfigDto.java
+++ march-configuration/src/main/java/io/github/march_plugin/configuration/dto/MarchConfigDto.java
@@ -1,9 +1,17 @@
 package io.github.march_plugin.configuration.dto;
 
+import io.github.march_plugin.configuration.dto.classification.ModulesDto;
 import io.github.march_plugin.configuration.dto.dimensions.DimensionDto;
+import io.github.march_plugin.configuration.dto.modularity.ProjectStructureDto;
+import io.github.march_plugin.configuration.dto.package_templates.PackageTemplatesDto;
+import io.github.march_plugin.configuration.dto.rules.RulesDto;
 
 import java.util.List;
 
 public record MarchConfigDto(
-        List<DimensionDto> dimensions
+        List<DimensionDto> dimensions,
+        ProjectStructureDto projectStructure,
+        PackageTemplatesDto packageTemplates,
+        ModulesDto modules,
+        RulesDto rules
 ) {}
